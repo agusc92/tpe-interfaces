6f9948e5c327daa0fcdc9f32eef190bc6b0e3208
Agustin <agustinlabattaglia92@gmail.com>
2024-11-21 16:56:25 -0300

    casi terminado, falta responsive

--- scripts/participa.js
+++ scripts/participa.js
@@ -2,6 +2,12 @@ let botonesPostular = [...document.querySelectorAll(".postularme")]
 let popup = document.querySelector('.perro-popup');
 let botonEnviar = document.querySelector("#enviar");
 let fondoFalso = document.getElementById("fondoFalso");
+let selecVoluntario = document.getElementById("voluntario");
+let selecHogar = document.getElementById("hogar");
+let mail = document.getElementById("mail");
+let errorMail = document.getElementById("errorMail");
+let errorRadio = document.getElementById("errorRadio");
+
 botonesPostular.forEach(boton =>{
     boton.addEventListener('click',()=>{
         popup.classList.remove('desaparecer');
@@ -11,11 +17,28 @@ botonesPostular.forEach(boton =>{
 )
 
 botonEnviar.addEventListener('click',()=>{
-    popup.classList.add('desaparecer')
-    fondoFalso.classList.add('desaparecer')
+    if(selecVoluntario.checked || selecHogar.checked){
+        errorRadio.classList.add("desaparecer");
+        if(mailValido(mail.value)){
+            errorMail.classList.remove("desaparecer");
+            popup.classList.add('desaparecer')
+            fondoFalso.classList.add('desaparecer')
+        }else{
+            errorMail.classList.remove("desaparecer");
+        }
+    }else{
+        errorRadio.classList.remove("desaparecer")
+    }
+    
+
 })
 fondoFalso.addEventListener('click',()=>{
     popup.classList.add('desaparecer');
     fondoFalso.classList.add('desaparecer');
     
 })
+
+function mailValido(mail) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(mail);
+  }
